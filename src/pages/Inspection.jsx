7a39fe863dd904f20cc1ad8c717a14946166ade3
Dominic <haakym@gmail.com>
2022-12-06 10:21:54 +0000

    get inspection data from the API

--- src/pages/Inspection.jsx
+++ src/pages/Inspection.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Inspection() {
   const params = useParams();
+  const [details, setDetails] = useState({});
+  const [loadError, setLoadError] = useState(false);
 
+  useEffect(() => {
+    axios.get(`http://localhost/api/inspections/${params.inspectionId}`)
+      .then((response) => {
+        if (response.data) {
+          setDetails(response.data)
+        }
+      }).catch((error) => {
+        console.error(error);
+        setLoadError(true);
+      });
+  }, []);
+
+  if (loadError) {
+    return (
+      <div>Cannot load data. Error contacting API.</div>
+    );
+  }
+  
   return (
-    <div>This is inspection {params.inspectionId} </div>
+    <div>This is inspection {params.inspectionId} for {details.id} </div>
   )
 }
